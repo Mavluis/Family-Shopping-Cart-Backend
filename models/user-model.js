dade1f4b051fbd6c3b673ccaea5150de1b1777ff
Luis <mavluis@gmail.com>
2019-06-14 13:00:29 +0200

    idproducts

---
+++ models/user-model.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const mysql = require('mysql2');
+
+const { Schema } = mysql;
+
+const userSchema = new Schema({
+  uuid: {
+    type: String,
+    unique: true,
+  },
+  fullName: String,
+  preferences: {
+    isPublicProfile: Boolean,
+    linkedIn: String,
+    twitter: String,
+    github: String,
+    description: String,
+  },
+});
+
+userSchema.index(
+  {
+    fullName: 'text',
+  },
+);
+
+const User = mysql.model('User', userSchema);
+
+module.exports = User;
