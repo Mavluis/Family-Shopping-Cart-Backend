6553926254de79884508802c33c341d57c21736e
Luis <mavluis@gmail.com>
2019-07-02 11:57:25 +0200

    createcartproducts

--- webserver/controllers/post/create-cart.js
+++ webserver/controllers/post/create-cart.js
@@ -4,29 +4,51 @@ const mysql = require('../../../databases/mysql-pool');
 const uuidV4 = require('uuid/v4');
 
 async function createCart(req, res, next) {
-
-  // 1: Take request data
+  
   const requestData = { ...req.body };
   const note = requestData.note;
-
-  // 2: Take the user ID from previews middleware
   const { uuid } = req.claims;
-  
-  // 3: Generate necessary data
   const cart_id = uuidV4();
-  const created_at = new Date()
+  const created_at = new Date();
   
-  console.log({ requestData, uuid, note, cart_id })
-  // 4: Insert into DB
   const sqlQuery = 'INSERT INTO carts SET ?';
   const connection = await mysql.getConnection();
-
+  
+  console.log({ requestData, uuid, note, cart_id })
+  console.log("System Failure!!!");
   try {
     await connection.query(sqlQuery, {
       note,
       cart_id,
       created_at,
       user_id: uuid,
+    });
+    connection.release();
+
+    return res.status(201).send();
+  } catch (e) {
+    return res.status(400).send(e);
+  }
+}
+
+module.exports = createCart;
+
+async function createcartProducts(req, res, next) {
+  
+  const requestData = { ...req.body };
+  const amount = requestData.amount;
+  const { uuid } = req.claims;
+  const product_id = uuidV4();
+
+  const sqlQuery = 'INSERT INTO cart_products SET ?';
+  const connection = await mysql.getConnection();
+
+  console.log({ requestData, uuid, product_id })
+  try {
+    await connection.query(sqlQuery, {
+      amount,
+      cart_id,
+      product_id
     });
 
     connection.release();
@@ -35,15 +57,11 @@ async function createCart(req, res, next) {
     return res.status(400).send(e);
   }
 
-
-
-
-  
-  try {
-    return res.status(201).send();
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
+  /*   try {
+      return res.status(201).send();
+    } catch (e) {
+      res.status(500).send(e.message);
+    } */
 }
 
-module.exports = createCart;
+module.exports = createcartProducts;
--- webserver/routes/cart-router.js
+++ webserver/routes/cart-router.js
@@ -3,11 +3,9 @@
 const express = require('express');
 const checkJwtToken = require('../controllers/session/check-jwt-token');
 const createCart = require('../controllers/post/create-cart');
-const createcartProducts = require('../controllers/post/create-cart-products');
 
 const router = express.Router();
 
 router.post('/create-cart', checkJwtToken, createCart);
-router.post('/create-cart_products', checkJwtToken, createcartProducts);
 
 module.exports = router;
