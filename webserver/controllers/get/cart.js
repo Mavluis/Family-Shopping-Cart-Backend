6452467f520b9be8b1aed7eabf7a475540050c5e
Luis <mavluis@gmail.com>
2019-07-05 18:45:42 +0200

    Select Note and Products Fail

--- webserver/controllers/get/cart.js
+++ webserver/controllers/get/cart.js
@@ -7,16 +7,16 @@ async function Cart(req, res, next) {
     try {
         const connection = await mysql.getConnection();
         const sqlQuery = `SELECT cart_id, name FROM cart_products 
-            INNER JOIN products ON cart_products.product_id = 
-            products.product_id`;
+        INNER JOIN products ON cart_products.product_id = 
+        products.product_id`;
         const [result] = await connection.query(sqlQuery);
 
-        const sqlQuery1 = `SELECT user_id, note FROM carts INNER JOIN 
-            users ON carts.user_id = users.uuid`;
-        const [result1] = await connection.query(sqlQuery1);
+        /* const sqlQuery1 = `SELECT user_id, note FROM carts INNER JOIN 
+        users ON carts.user_id = users.uuid`;
+        const [result1] = await connection.query(sqlQuery1); */
 
         console.log(result);
-        console.log(result1);
+        /* console.log(result1); */
 
         console.log("System Failure!!!");
 
