362e39336feb4b5d07ec54047f103ad0efa29dfe
Luis <mavluis@gmail.com>
2019-07-05 22:51:52 +0200

    Select Note and Products OK

--- webserver/controllers/get/cart.js
+++ webserver/controllers/get/cart.js
@@ -5,25 +5,25 @@ const mysql = require('../../../databases/mysql-pool');
 async function Cart(req, res, next) {
 
     try {
+
         const connection = await mysql.getConnection();
-        const sqlQuery = `SELECT cart_id, name FROM cart_products 
+        const sqlQuery = `SELECT name FROM cart_products 
         INNER JOIN products ON cart_products.product_id = 
-        products.product_id`;
+        products.product_id WHERE cart_id = '6850cccb-df85-4bd4-83ba-f15f087e5431'`;
         const [result] = await connection.query(sqlQuery);
 
-        /* const sqlQuery1 = `SELECT user_id, note FROM carts INNER JOIN 
-        users ON carts.user_id = users.uuid`;
-        const [result1] = await connection.query(sqlQuery1); */
-
+        const sqlQuery1 = `SELECT note FROM carts 
+        WHERE user_id = '4debe0f9-4835-46c8-bd10-a056d5859409'`;
+        const [result1] = await connection.query(sqlQuery1);
+        
         console.log(result);
-        /* console.log(result1); */
-
-        console.log("System Failure!!!");
-
+        console.log(result1);
+        
         connection.release();
         return res.status(201).send();
     } catch (e) {
         return res.status(400).send(e);
+        console.log("System Failure!!!");
     }
 }
 
