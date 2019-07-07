1d930bbcfcea6d724c775ff5388664d09884bb05
Luis <mavluis@gmail.com>
2019-07-07 20:03:38 +0200

    NOK cart_id = cart

--- webserver/controllers/get/cart.js
+++ webserver/controllers/get/cart.js
@@ -5,28 +5,29 @@ const mysql = require('../../../databases/mysql-pool');
 async function Cart(req, res, next) {
 
     const accountData = { ...req.body };
-    console.log(accountData);
+
     try {
         const connection = await mysql.getConnection();
-        const sqlQueryUser = `SELECT uuid FROM users 
+        const sqlQueryUuidNote = `SELECT note FROM users 
+        INNER JOIN carts ON users.uuid = carts.user_id 
         WHERE email = '${accountData.email}'`;
-        const [uuid] = await connection.query(sqlQueryUser);
-        console.log(uuid);
+        const [note] = await connection.query(sqlQueryUuidNote);
 
-        const userId = [uuid];
-        console.log(userId);
+        const sqlQueryCart = `SELECT cart_id FROM carts 
+        INNER JOIN users ON users.uuid = 
+        carts.user_id WHERE email = '${accountData.email}'`;
+        const [cart] = await connection.query(sqlQueryCart);
+        console.log(cart);
 
-        const sqlQueryNote = `SELECT note FROM carts 
-            WHERE cart_id = '${userId}'`;
-        const [note] = await connection.query(sqlQueryNote);
-        console.log(note);
+/* [ TextRow { cart_id: '6850cccb-df85-4bd4-83ba-f15f087e5431' } ] */
 
         const sqlQueryName = `SELECT name FROM cart_products 
-            INNER JOIN products ON cart_products.product_id = 
-            products.product_id WHERE cart_id = '${userId}'`;
+        INNER JOIN products ON cart_products.product_id = 
+        products.product_id WHERE cart_id = "${cart}"`;
         const [name] = await connection.query(sqlQueryName);
+        console.log({name});
 
-        const cartProducts = name.concat(note);
+        const cartProducts = note.concat(name);
         console.log(cartProducts);
 
         connection.release();
