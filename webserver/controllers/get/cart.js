bbd25cb8c782405c9717a96d45ae6ceba28732a6
Luis <mavluis@gmail.com>
2019-07-08 18:19:32 +0200

    OK cart_id = cart and order by date last

--- webserver/controllers/get/cart.js
+++ webserver/controllers/get/cart.js
@@ -10,22 +10,20 @@ async function Cart(req, res, next) {
         const connection = await mysql.getConnection();
         const sqlQueryUuidNote = `SELECT note FROM users 
         INNER JOIN carts ON users.uuid = carts.user_id 
-        WHERE email = '${accountData.email}'`;
+        WHERE email = '${accountData.email}' 
+        ORDER BY carts.created_at DESC LIMIT 1`;
         const [note] = await connection.query(sqlQueryUuidNote);
 
         const sqlQueryCart = `SELECT cart_id FROM carts 
         INNER JOIN users ON users.uuid = 
-        carts.user_id WHERE email = '${accountData.email}'`;
+        carts.user_id WHERE email = '${accountData.email}' 
+        ORDER BY carts.created_at DESC LIMIT 1`;
         const [cart] = await connection.query(sqlQueryCart);
-        console.log(cart);
-
-/* [ TextRow { cart_id: '6850cccb-df85-4bd4-83ba-f15f087e5431' } ] */
 
         const sqlQueryName = `SELECT name FROM cart_products 
         INNER JOIN products ON cart_products.product_id = 
-        products.product_id WHERE cart_id = "${cart}"`;
+        products.product_id WHERE cart_id = "${cart[0].cart_id}"`;
         const [name] = await connection.query(sqlQueryName);
-        console.log({name});
 
         const cartProducts = note.concat(name);
         console.log(cartProducts);
